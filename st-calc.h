fcffa327ce0af63f308ae70a5d496f8fd275e372
Xin Li <lixin.reco@gmail.com>
2020-10-17 20:09:22 +0200

    updated license

--- st-calc.h
+++ st-calc.h
@@ -1,22 +1,22 @@
-/*****************************************************************************
- This file is part of the SurfTensCalc program.                                      
- Copyright (C) 2012 Xin Li
-                                                                           
- Filename:  st-calc.h
- License:   BSD 2-Clause License
+/*
+ *  This file is part of the SurfTensCalc program.
+ *  Copyright (C) 2012 Xin Li
 
- This software is provided by the copyright holders and contributors "as is"
- and any express or implied warranties, including, but not limited to, the
- implied warranties of merchantability and fitness for a particular purpose are
- disclaimed. In no event shall the copyright holder or contributors be liable
- for any direct, indirect, incidental, special, exemplary, or consequential
- damages (including, but not limited to, procurement of substitute goods or
- services; loss of use, data, or profits; or business interruption) however
- caused and on any theory of liability, whether in contract, strict liability,
- or tort (including negligence or otherwise) arising in any way out of the use
- of this software, even if advised of the possibility of such damage.
- *****************************************************************************/
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *  USA
+ */
 
    const double pi   =   3.14159265358979323846;
    const double nA   =   6.02214129 ;  /* 10^23 mol^-1 */
